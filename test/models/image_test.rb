c2ec7988e38431777ca7f7d8460071ad1988a485
David Weinflash <david.weinflash@appfolio.com>
2020-06-25 14:36:39 -0700

    Weinflash - Make image models taggable

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -21,4 +21,31 @@ class ImageTest < ActiveSupport::TestCase
     Rails.application.load_seed
     assert_equal Image.count, 10
   end
+
+  def test_tag
+    valid_image_url = 'https://www.gstatic.com/webp/gallery3/1.png'
+    tag = 'tag1'
+    image = Image.create!(url: valid_image_url, tag_list: tag)
+    assert_equal image.tag_list, [tag]
+  end
+
+  def test_tags
+    valid_image_url = 'https://www.gstatic.com/webp/gallery3/1.png'
+    tags = 'tag1,tag2'
+    image = Image.create!(url: valid_image_url, tag_list: tags)
+    assert_equal image.tag_list, %w(tag1 tag2)
+  end
+
+  def test_empty_tag
+    valid_image_url = 'https://www.gstatic.com/webp/gallery3/1.png'
+    tag = ''
+    image = Image.create!(url: valid_image_url, tag_list: tag)
+    assert_equal image.tag_list, []
+  end
+
+  def test_no_tag
+    valid_image_url = 'https://www.gstatic.com/webp/gallery3/1.png'
+    image = Image.create!(url: valid_image_url, tag_list: nil)
+    assert_equal image.tag_list, []
+  end
 end
