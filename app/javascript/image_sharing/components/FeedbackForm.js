63d81b481d05f81ac86304525149cbbb2ad4b353
David Weinflash <david.weinflash@appfolio.com>
2020-07-09 16:54:46 -0700

    Weinflash - FeedbackForm

--- app/javascript/image_sharing/components/FeedbackForm.js
+++ app/javascript/image_sharing/components/FeedbackForm.js
@@ -1,23 +1,49 @@
 import React, { Component } from 'react';
-import { Col, Button, Input, Form, Label, FormGroup } from 'reactstrap';
+import { observer } from 'mobx-react';
+import { Col, Button, Input, Form, Label, FormGroup, Alert } from 'reactstrap';
+import PostFeedbackService from '../services/PostFeedbackService';
+import PropTypes from 'prop-types';
 
+@observer
 class FeedbackForm extends Component {
+  static propTypes = {
+    store: PropTypes.object.isRequired
+  };
+
+  onClickHandler = () => {
+    const service = new PostFeedbackService(this.props.store);
+    service.postFeedback();
+  };
+
   render() {
     return (
-      <Form>
-        <Col lg={{ size: 4, offset: 4 }}>
-          <FormGroup>
-            <Label> Your name: </Label>
-            <Input className='js-username' type="text" />
-          </FormGroup>
-          <FormGroup>
-            <Label> Comments: </Label>
-            <Input className='js-comments' type="textarea" />
-          </FormGroup>
-          <Button type="button" >Submit</Button>
-        </Col>
-      </Form>
-    )
+      <div>
+        {this.props.store.response !== '' && <Alert>{this.props.store.response}</Alert>}
+        <Form>
+          <Col lg={{size: 4, offset: 4}}>
+            <FormGroup>
+              <Label> Your name: </Label>
+              <Input
+                className='js-username'
+                type="text"
+                onChange={e => this.props.store.setUserName(e.target.value)}
+                value={this.props.store.userName}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label> Comments: </Label>
+              <Input
+                className='js-comments'
+                type="textarea"
+                onChange={e => this.props.store.setComments(e.target.value)}
+                value={this.props.store.comments}
+              />
+            </FormGroup>
+            <Button type="button" color='primary' onClick={this.onClickHandler}>Submit</Button>
+          </Col>
+        </Form>
+      </div>
+    );
   }
 }
 
